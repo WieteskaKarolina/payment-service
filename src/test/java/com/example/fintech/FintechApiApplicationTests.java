fc78005afafac6055d88ebb0d975ca4a87119089
WieteskaKarolina <wieteskakarolina02@gmail.com>
2026-09-08 11:54:03 +0200

    Initial Spring Boot project

---
+++ src/test/java/com/example/fintech/FintechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
